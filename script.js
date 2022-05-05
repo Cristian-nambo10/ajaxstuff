e550affed7a5b829be7b3e7060c9edb4ca79a342
Cristian Nambo <nambocristian10@gmail.com>
2022-05-05 15:47:10 -0500

    work in progress: we can perform ajax ... minimum functionality

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// alert('Were good')
+// How to request data from an API using javascript and jQuery ...
+
+// There are many ways to do this! EX: ...
+// 1. jQuerys $.ajax() function ...
+// 2. The Browsers fetch() function ...
+// 3. Using the Axios http client library ...
+
+const url = 'http://www.omdbapi.com/?apikey=c3ff07d0&t=Die+Hard';
+
+$.ajax(url).then(function(data) {
+    console.log('Movie data is ready')
+    console.log(data)
+}), function() {
+    console.log('Something is wrong')
+    console.log(error)    // This is error handling
+}
